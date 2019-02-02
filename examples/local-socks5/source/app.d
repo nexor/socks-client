e316d2a13eb846e2210d0600f42571647de65774
Pavel Chebotarev <chebotarevp@gmail.com>
2019-02-02 22:16:41 +0300

    Multiple drivers support, improved local-socks example

--- examples/local-socks5/source/app.d
+++ examples/local-socks5/source/app.d
@@ -1,16 +1,20 @@
 import vibe.core.net;
 import vibe.core.log;
 import vibe.core.core;
-import std.typecons;
 import socks.client.vibe;
 import std.stdio;
+
+string proxyHost = "127.0.0.1";
+ushort proxyPort = 1080;
 
 // connect to dlang.org through local socks5 proxy
 int main(string[] args)
 {
+    parseArgs(args);
+
     Socks5Options socks5Options = {
-        host: "127.0.0.1",
-        port: 1080,
+        host: proxyHost,
+        port: proxyPort,
     };
 
     TCPConnection conn = connectTCPSocks(socks5Options, "dlang.org", 80);
@@ -31,4 +35,14 @@ int main(string[] args)
     writefln("%s", cast(char[])chunk);
 
     return 0;
+}
+
+void parseArgs(string[] args)
+{
+    import std.conv : to;
+
+    if (args.length == 3) {
+        proxyHost = args[1];
+        proxyPort = args[2].to!ushort;
+    }
 }
